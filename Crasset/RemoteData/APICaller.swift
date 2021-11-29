467d819648327a208aa19920fc3f043f08cfdd9a
dave1b <57140925+dave1b@users.noreply.github.com>
2021-11-30 00:19:01 +0100

    implemented second method for api-request

--- Crasset/RemoteData/APICaller.swift
+++ Crasset/RemoteData/APICaller.swift
@@ -8,16 +8,20 @@
 import Foundation
 import SwiftUI
 
-@MainActor class APICaller: ObservableObject {
+class APICaller: ObservableObject {
     static let apiURL = "https://api.coingecko.com/api/v3/"
+    static let apiURL2 = "https://min-api.cryptocompare.com/data/price?fsym="
     @Published var cryptos = [Crypto]()
     @Published var crypto: Crypto? = nil
+    @Published var cryptoFiat: CryptoFiat? = nil
     
-    func getSingleDetailsCrypto(cryptoID: String, currencyID: String, completionHandler: @escaping ([Crypto]) -> Void) {
-        let url = URL(string: "\(APICaller.apiURL)coins/markets?vs_currency=\(currencyID)ids=\(cryptoID)&order=market_cap_desc&per_page=1&page=1&sparkline=false")!
+    
+    
+    func getSingleDetailsCrypto(cryptoID: String, completionHandler: @escaping (CryptoFiat) -> Void) {
+        let url = URL(string: "\(APICaller.apiURL2)\(cryptoID)&tsyms=USD,CHF,EUR")!
         let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
             if let error = error {
-                print("Error with fetching crypo: \(error)")
+                print("Error with fetching cryptos: \(error)")
                 return
             }
             guard let httpResponse = response as? HTTPURLResponse,
@@ -26,22 +30,37 @@ import SwiftUI
                       return
                   }
             if let data = data,
-                let crypto = try? JSONDecoder().decode([Crypto].self, from: data) {
-                print(crypto)
-                completionHandler(crypto)
+               let cryptoFiat = try? JSONDecoder().decode(CryptoFiat.self, from: data) {
+                print(cryptoFiat)
+                completionHandler(cryptoFiat)
             }
         })
         task.resume()
     }
     
+    /*
+     func getSingleDetailsCrypto(cryptoID: String, currencyID: String, completionHandler: @escaping (CryptoFiat) -> Void) {
+     let url = URL(string: "\(APICaller.apiURL)simple/price?ids=\(cryptoID)&vs_currencies=\(currencyID)")!
+     let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+     if let error = error {
+     print("Error with fetching crypo: \(error)")
+     return
+     }
+     guard let httpResponse = response as? HTTPURLResponse,
+     (200...299).contains(httpResponse.statusCode) else {
+     print("Error with the response, unexpected status code: \(String(describing: response))")
+     return
+     }
+     if let data = data,
+     let cryptoFiat = try? JSONDecoder().decode(CryptoFiat.self, from: data) {
+     print(cryptoFiat)
+     completionHandler(cryptoFiat)
+     }
+     })
+     task.resume()
+     }*/
     
-    
-    
-    
-    
-    
-    
-    func getAllCryptos(currencyID: String, completionHandler: @escaping ([Crypto]) -> Void) {
+    func getAllCryptos(currencyID: String, completionHandler: @escaping ([Crypto]) -> Void) async {
         var url: String = "\(APICaller.apiURL)coins/markets?vs_currency=\(currencyID)&ids="
         for crypto in SupportedCrypto.getsupportedCryptoArray() {
             url = "\(url)\(crypto)%2C%20"
