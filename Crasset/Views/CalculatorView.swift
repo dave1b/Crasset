27f26e91fddb0eedb1d900b57a08cfb26283a2f8
dave1b <57140925+dave1b@users.noreply.github.com>
2021-11-30 00:20:36 +0100

    implemented logic for calculating

--- Crasset/Views/CalculatorView.swift
+++ Crasset/Views/CalculatorView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct CalculatorView: View {
-    @State var crypto: [Crypto]?
-    @State private var amount1 = ""
-    @State private var amount2 = ""
-    @State private var pickedCurrency1 = ""
-    @State private var pickedCurrency2 = ""
+    @State var cryptoFiat1: CryptoFiat?
+    @State var cryptoFiat2: CryptoFiat?
+    @State private var amount1: String = ""
+    @State private var amount2: String = ""
+    @State private var pickedCurrency1 = "USD"
+    @State private var pickedCurrency2 = "USD"
     
     let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
-    
     
     var body: some View {
         
@@ -26,6 +26,7 @@ struct CalculatorView: View {
                     TextField(
                         "Currency 1",
                         text: $amount1)
+                        .keyboardType(.numberPad)
                         .onChange(of: amount1) { newValue in
                             if newValue == amount1 {
                                 amount1Changed()
@@ -36,7 +37,7 @@ struct CalculatorView: View {
                         .background(lightGreyColor)
                         .cornerRadius(5.0)
                         .padding()
-                    CurrencyPicker(pickedCurrency: $pickedCurrency1)
+                    CurrencyPicker(parentView:  self , pickedCurrency: $pickedCurrency1)
                         .frame(width: 100.0, height: 25.0, alignment: .center)
                 }
                 
@@ -46,6 +47,7 @@ struct CalculatorView: View {
                     TextField(
                         "Currency 2",
                         text: $amount2)
+                        .keyboardType(.numberPad)
                         .onChange(of: amount1) { newValue in
                             if newValue == amount1 {
                                 amount1Changed()
@@ -55,36 +57,40 @@ struct CalculatorView: View {
                         .background(lightGreyColor)
                         .cornerRadius(5.0)
                         .padding()
-                    CurrencyPicker(pickedCurrency: $pickedCurrency2)
+                    CurrencyPicker(parentView:  self ,pickedCurrency: $pickedCurrency2)
                         .frame(width: 100.0, height: 25.0, alignment: .center)
                 }
                 
                 Spacer()
                 
-                
-                
-                
-                
-                
-                
-            }.navigationTitle("Crypto Calculator")
+            }.task {
+                APICaller().getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: pickedCurrency1)){ (response) in
+                    cryptoFiat1 = response
+                    //print(cryptoFiat1)
+                }
+                APICaller().getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: pickedCurrency2)){ (response) in
+                    self.cryptoFiat2 = response
+                }
+            }
+            .navigationTitle("Crypto Calculator")
         }
     }
     
-    @MainActor func amount1Changed() {
-        APICaller().getSingleDetailsCrypto(cryptoID: "bitcoin", currencyID: "usd"){ cryptos in
-            self.crypto = cryptos
+    func amount1Changed() {
+        APICaller().getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: pickedCurrency1)){ (response) in
+            self.cryptoFiat1 = response
         }
-        amount2 = String(crypto?[0].usd[0] ?? 2.0)
-        print("Name changed to \(amount1)!")
+        APICaller().getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: pickedCurrency2)){ (response) in
+            self.cryptoFiat2 = response
+        }
+        let amount1AsFloat: Float = (Float(amount1) ?? 0.0)
+        amount2 = String(format: "%.2f", amount1AsFloat * (cryptoFiat1?.USD ?? 0.0) / (cryptoFiat2?.USD ?? 0.0))
+        print(amount2)
     }
     func amount2Changed() {
-        print("Name changed to \(amount2)!")
+        // unimplemented
     }
-    
 }
-
-
 
 struct CalculatorView_Previews: PreviewProvider {
     static var previews: some View {
