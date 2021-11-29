543121ef3649126022d2f24003a0954bd46a7fd1
dave1b <57140925+dave1b@users.noreply.github.com>
2021-11-30 00:18:35 +0100

    new struct for CryptoData

---
+++ Crasset/Data/CryptoFiat.swift
@@ -0,0 +1,14 @@
+//
+//  CryptoFiat.swift
+//  Crasset
+//
+//  Created by Dave Brunner on 29.11.21.
+//
+
+import Foundation
+
+struct CryptoFiat: Decodable  {
+    let USD: Float?
+    let EUR: Float?
+    let CHF: Float?
+}
