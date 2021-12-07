5b67e2d0e3d3321eecbea85cc3dbdca48e866a95
dave1b <57140925+dave1b@users.noreply.github.com>
2021-12-07 17:35:46 +0100

    fix: fixed calculating  issue

--- Crasset/Views/CalculatorView.swift
+++ Crasset/Views/CalculatorView.swift
@@ -14,7 +14,8 @@ struct CalculatorView: View {
     @State private var amount2: String = "0.0"
     @State private var pickedCurrency1 = "USD"
     @State private var pickedCurrency2 = "USD"
-    @State private var flag = false
+    @State private var isFocused1 = false
+    @State private var isFocused2 = false
 
     
     let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
@@ -27,10 +28,12 @@ struct CalculatorView: View {
                 VStack{
                     TextField(
                         "Currency 1",
-                        text: $amount1)
+                        text: $amount1, onEditingChanged: { (changed) in
+                            isFocused1 = changed
+                        })
                         .keyboardType(.numberPad)
                         .onChange(of: amount1) { newValue in
-                            if newValue == amount1 {
+                            if isFocused1 {
                                 amount1Changed()
                             }
                         }
@@ -39,6 +42,7 @@ struct CalculatorView: View {
                         .background(lightGreyColor)
                         .cornerRadius(5.0)
                         .padding()
+                        .padding([.bottom], 25.0)
                     CurrencyPicker(pickedCurrency: $pickedCurrency1)
                         .frame(width: 100.0, height: 25.0, alignment: .center)
                         .onChange(of: pickedCurrency1, perform: { value in
@@ -51,17 +55,21 @@ struct CalculatorView: View {
                 VStack{
                     TextField(
                         "Currency 2",
-                        text: $amount2)
+                        text: $amount2, onEditingChanged: { (changed) in
+                            isFocused2 = changed
+                        })
                         .keyboardType(.numberPad)
-                        .onChange(of: amount1) { newValue in
-                            if newValue == amount1 {
-                                amount1Changed()
+                        .onChange(of: amount2) { newValue in
+                            if isFocused2 {
+                                amount2Changed()
                             }
                         }
                         .padding()
                         .background(lightGreyColor)
                         .cornerRadius(5.0)
                         .padding()
+                        .padding([.bottom], 25.0)
+
                     CurrencyPicker(pickedCurrency: $pickedCurrency2)
                         .frame(width: 100.0, height: 25.0, alignment: .center)
                         .onChange(of: pickedCurrency2, perform: { value in
@@ -88,36 +96,27 @@ struct CalculatorView: View {
     func picker1Changed() {
         APICaller.getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: pickedCurrency1)){ (response) in
             cryptoFiat1 = response
-            amount1Changed()
+            amount2Changed()
         }
     }
     
     func picker2Changed(){
         APICaller.getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: pickedCurrency2)){ (response) in
             cryptoFiat2 = response
-            amount2Changed()
+            amount1Changed()
         }
     }
     
-    
     func amount1Changed() {
-        if(flag == false){
-        flag = true
-        let amount1AsFloat: Float = (Float(amount1) ?? 0.0)
+        let amount1AsFloat: Float = (Float(amount1) ?? 0)
         amount2 = String(format: "%.2f", amount1AsFloat * (cryptoFiat1?.USD ?? 0.0) / (cryptoFiat2?.USD ?? 0.0))
         print(amount2)
-        flag = false
-        }
     }
     
     func amount2Changed() {
-        if(flag == false){
-        flag = true
-        let amount2AsFloat: Float = (Float(amount2) ?? 0.0)
+        let amount2AsFloat: Float = (Float(amount2) ?? 0)
         amount1 = String(format: "%.2f", amount2AsFloat * (cryptoFiat2?.USD ?? 0.0) / (cryptoFiat1?.USD ?? 0.0))
         print(amount1)
-        flag = false
-        }
     }
 }
 
