bf27fbd8fbe44a2aa7236ce1e557d75d4220357f
dave1b <57140925+dave1b@users.noreply.github.com>
2021-12-07 17:37:54 +0100

    added new property to ChartData

--- Crasset/Views/Chart/ChartDataContainer.swift
+++ Crasset/Views/Chart/ChartDataContainer.swift
@@ -13,7 +13,7 @@ class ChartDataContainer : ObservableObject {
     let colorArray: [Color] = [Color(#colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)),Color(#colorLiteral(red: 0.9361220002, green: 0.9322158694, blue: 0, alpha: 1)),Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)),Color(#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)),Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)),Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)),Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1))]
     let emptyColor = Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
     private var service: CoinCoreDataService?
-    @Published var chartData : [ChartData] = [ChartData(color: Color(#colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)), percent: 10, value: 10)]
+    @Published var chartData : [ChartData] = [ChartData(color: Color(#colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)), percent: 10, value: 10, coinID: "unknown")]
     
 
     func updateCoreData(service: CoinCoreDataService) {
@@ -30,17 +30,15 @@ class ChartDataContainer : ObservableObject {
         let group = DispatchGroup()
         for crypto in strongService.portfolio {
             group.enter()
-            APICaller().getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: crypto.coinID ?? "")){ [weak self] (response) in
+            APICaller.getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: crypto.coinID ?? "")){ [weak self] (response) in
                 guard let self = self else {
                     return
                 }
-                DispatchQueue.main.async{
                     let coinData = response
                     let priceInUSD = coinData.USD ?? 0.0
                     self.totalInUSD += priceInUSD * crypto.amount
                     print(self.totalInUSD)
                     group.leave()
-                }
                 
             }
         }
@@ -48,18 +46,17 @@ class ChartDataContainer : ObservableObject {
 
         group.notify(queue: .main) {
             for asset in strongService.portfolio {
-                APICaller().getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: asset.coinID ?? "")){ [weak self] (response) in
+                APICaller.getSingleDetailsCrypto(cryptoID: SupportedCrypto.getCryptoKeyForAPI(key: asset.coinID ?? "")){ [weak self] (response) in
                     guard let self = self else {
                         return
                     }
-                    DispatchQueue.main.async{
                         let coinData = response
                         let priceInUSD = coinData.USD ?? 0.0
                         print("usd", priceInUSD)
-                        self.chartData.append(ChartData(color: self.colorArray[index%6], percent: (CGFloat(asset.amount * priceInUSD / self.totalInUSD) * 100), value: (CGFloat(asset.amount * priceInUSD / self.totalInUSD) * 100)))
+                    self.chartData.append(ChartData(color: self.colorArray[index%6], percent: (CGFloat(asset.amount * priceInUSD / self.totalInUSD) * 100), value: (CGFloat(asset.amount * priceInUSD / self.totalInUSD) * 100), coinID:  asset.coinID ?? "unknown"))
                         self.calc()
                         index = index+1
-                    }
+                    
 
                 }
                 
